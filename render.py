309decc3847a61619557c71628ad2123240fb48f
Weezl <alch3mist94@protonmail.com>
2021-09-03 23:08:04 +0200

    cosmetics

--- render.py
+++ render.py
@@ -5,38 +5,47 @@ from random import choice
 
 
 def image(path, color=True, floor=[0,0,0], printer=False, shade="-~%9@###", filled=False):
+
+    '''
+    Renders the image specified by path in terminal. The adaption to the terminal 
+    happens by coarse graining a common mean field method to derive a transformed 
+    (lower resolved) lattice. To increase the performance the integration of colors 
+    per segments is achieved by a stratified integration (Montecarlo estimate).
+
+    Arguments
+    - path [string]: path to image file
+    - color [boolean]: poly-to-monochromatic switch (if disabled the filled arg will not work instead ASCII will be used)
+    - floor [list]: A 3-vector RGB filter which will be added to the actual color. Each element can be a number between 0-255.
+    - printer [boolean]: Outputs the rendered image directly in terminal when enabled, but generally returns it.
+    - shade [string]: A 0-10 length string for texture shading. From dark to bright = small to large ASCII surface.
+    - filled [boolean]: Use the max. ASCII span. Good for "HD" images, but works only if color is enabled.
+    '''
+
     with Image.open(path) as img:
         # get geometry of image and terminal
         map = img.load()
-        w, h = img.size[0], img.size[1]
-        ycorr = 2
-        wt = get_terminal_size()[0]
+        w, h, ycorr, wt = img.size[0], img.size[1], 2, get_terminal_size()[0]
         if wt > w: wt=w
-        ht = int(h*wt/(w*ycorr))
         if w/wt < 2: stepX = 2
         else: stepX = int(w/wt)+1
         stepY = int(ycorr*stepX)
         rngX, rngY, step_inv, white_inv = range(stepX), range(stepY), 1./stepX, 1/25.5
-        if stepX < stepY: r = int(stepX/2) 
-        else: r = int(stepY/2)
         if filled and color: shade="█"
         ascii, shade_length = '', len(shade)
-        #print(f'image size: {w}x{h}px\nterminal size: {wt}x{ht}px')
         for y in range(0,h,stepY):
             for x in range(0,w,stepX):
-                # sample a few pixel to estimate the color
-                col = [0,0,0]
+                c = [0,0,0]
                 for i in range(stepX):
                     try:
                         m = map[x+choice(rngX), y+choice(rngY)]
+                        for j in range(3): c[j] += int(m[j])
                     except:
-                        pass
-                    for j in range(3): col[j] += int(m[j])
-                for i in range(3): col[i] = int(col[i]/stepX)
-                intensity_mean = int((0.3333*sum(col)*step_inv)*white_inv)
+                        i -= 1
+                for i in range(3): c[i] = int(c[i]*step_inv)
+                intensity_mean = int(0.3333*sum(c)*step_inv*white_inv)
                 if color:
-                    ascii += "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format((col[0]+floor[0])%255, 
-                    (col[1]+floor[1])%255, (col[2]+floor[2])%255, shade[intensity_mean%shade_length])
+                    ascii += "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format((c[0]+floor[0])%255, 
+                    (c[1]+floor[1])%255, (c[2]+floor[2])%255, shade[intensity_mean%shade_length])
                 else: ascii += shade[intensity_mean%shade_length]
             ascii += '\n'
         if printer: print(ascii)
