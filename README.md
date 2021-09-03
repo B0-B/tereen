e86c458b6685628d0bfa8aeb7197ae332af65403
Weezl <alch3mist94@protonmail.com>
2021-09-04 00:58:34 +0200

    added new halo animation for showcase and completed comments arround gif()

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# RENDER ENGINE
-A barebone image to ASCII converter which adapts to unix terminal size.
+<h2 align=center><strong>RENDER ENGINE</strong></h2>
 
+<p align=center>A barebone image to ASCII converter which adapts to unix terminal size.</p>
+
+<h1 align=center><img src='halo.gif'></h1>
+
+<h2 align=center><strong>SHOWCASE</strong></h2>
 <img src='cyber.png'>
 
 <br>
@@ -10,17 +14,24 @@ A barebone image to ASCII converter which adapts to unix terminal size.
 <br>
 <br>
 
-# Usage
+<h2 align=center><strong>USAGE</strong></h2>
+
 Set the terminal to the most optimal size
+
 ```bash
 render.py think.jpg
 ```
 <img src='think.png'>
 
+
+
 Import allows to use the module in full fashion
 
 ```python
-from render import image
+from render import image, gif
+
 # print with filled ascii (better for high resolution images)
 image('cyber.png', printer=True, filled=True)
+# ..
+gif('halo.gif', loop=True)
 ```
--- render.py
+++ render.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-from PIL import Image
+from PIL import Image, ImageSequence
 from shutil import get_terminal_size
 from random import choice
+from time import sleep
+from os import system as shell
 
 
-def image(path, color=True, floor=[0,0,0], printer=False, shade="-~%9@###", filled=False):
+def image(path=None, imageObject=None, color=True, floor=[0,0,0], printer=False, shade=".-~%9@###", filled=False):
 
     '''
     Renders the image specified by path in terminal. The adaption to the terminal 
@@ -21,35 +23,65 @@ def image(path, color=True, floor=[0,0,0], printer=False, shade="-~%9@###", fill
     - filled [boolean]: Use the max. ASCII span. Good for "HD" images, but works only if color is enabled.
     '''
 
-    with Image.open(path) as img:
-        # get geometry of image and terminal
-        map = img.load()
-        w, h, ycorr, wt = img.size[0], img.size[1], 2, get_terminal_size()[0]
-        if wt > w: wt=w
-        if w/wt < 2: stepX = 2
-        else: stepX = int(w/wt)+1
-        stepY = int(ycorr*stepX)
-        rngX, rngY, step_inv, white_inv = range(stepX), range(stepY), 1./stepX, 1/25.5
-        if filled and color: shade="█"
-        ascii, shade_length = '', len(shade)
-        for y in range(0,h,stepY):
-            for x in range(0,w,stepX):
-                c = [0,0,0]
-                for i in range(stepX):
-                    try:
-                        m = map[x+choice(rngX), y+choice(rngY)]
-                        for j in range(3): c[j] += int(m[j])
-                    except:
-                        i -= 1
-                for i in range(3): c[i] = int(c[i]*step_inv)
-                intensity_mean = int(0.3333*sum(c)*step_inv*white_inv)
-                if color:
-                    ascii += "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format((c[0]+floor[0])%255, 
-                    (c[1]+floor[1])%255, (c[2]+floor[2])%255, shade[intensity_mean%shade_length])
-                else: ascii += shade[intensity_mean%shade_length]
-            ascii += '\n'
-        if printer: print(ascii)
+    # get geometry of image and terminal
+    if imageObject != None and path == None:
+        img, map = imageObject, imageObject.load()
+    else:
+        with Image.open(path) as img:
+            map = img.load()
+    w, h, ycorr, wt = img.size[0], img.size[1], 2, get_terminal_size()[0]
+    if wt > w: wt=w
+    if w/wt < 2: stepX = 2
+    else: stepX = int(w/wt)+1
+    stepY = int(ycorr*stepX)
+    rngX, rngY, step_inv, white_inv = range(stepX), range(stepY), 1./stepX, 1/25.5
+    if filled and color: shade="█"
+    ascii, shade_length = '', len(shade)
+    for y in range(0,h,stepY):
+        for x in range(0,w,stepX):
+            c = [0,0,0]
+            for i in range(stepX):
+                try:
+                    m = map[x+choice(rngX), y+choice(rngY)]
+                    for j in range(3): c[j] += int(m[j])
+                except:
+                    i -= 1
+            for i in range(3): c[i] = int(c[i]*step_inv)
+            intensity_mean = int(0.3333*sum(c)*step_inv*white_inv)
+            if color:
+                ascii += "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format((c[0]+floor[0])%255, 
+                (c[1]+floor[1])%255, (c[2]+floor[2])%255, shade[intensity_mean%shade_length])
+            else: ascii += shade[intensity_mean%shade_length]
+        ascii += '\n'
+    if printer: print(ascii)
     return ascii
+
+def gif(path, filled=True, delay=0.1, loop=False, **kwargs):
+    
+    '''
+    Renders a gif animation and plays it directly in terminal. 
+    
+    - filled [boolean]: Argument is True by default.
+    - delay [float]: The time between frames in seconds.
+    - loop [boolean]: Will loop the animation until a keyboard interrupt.
+    - **kwargs: Same as image.
+    '''
+    
+    d, dias = abs(delay), [] # collect image dias
+    print(f'loading {path} ...')
+    im = Image.open(path)
+    for frame in ImageSequence.Iterator(im):
+        dias.append(
+            image(imageObject=frame.convert('RGB'), filled=True, **kwargs))
+    while True:
+        try:
+            for t in range(len(dias)):
+                print(dias[t])
+                sleep(d)
+        except KeyboardInterrupt:
+            print(' interrupt.')
+            break
+        if not loop: break
 
 if __name__ == '__main__':
     import argparse
@@ -57,4 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('file', type=argparse.FileType('r'))
     args = parser.parse_args()
     path = args.file.name
-    image(path, printer=True, filled=True)
+    if '.gif' in path.lower():
+        gif(path, filled=True)
+    else:
+        image(path, printer=True, filled=True)#, floor=[20,20,20])
